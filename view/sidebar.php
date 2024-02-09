8005f5fe7b3d0c0e865e1e708622392add5e2397
pkbmisesw <pkbmisesw@gmail.com>
2024-02-09 10:02:08 +0800

    rubah sidebar

--- view/sidebar.php
+++ view/sidebar.php
@@ -1,9 +1,8 @@
 <br>
 <p>Menu</p>
 <a href="../admin">1.Dashboard</a>
-<a href="../m_user">2.Master Pengguna</a>
-<a href="../m_surat">3.Master Surat</a>
-<!-- <a href="../m_pages">Master Pages</a> -->
+<a href="../m_user">2.Pengguna</a>
+<a href="../m_surat">3.Surat</a>
 <a href="../m_pages">4.Pages</a>
 <a href="../m_subpages">5.Sub Pages</a>
 <a href="../m_post">6.Post</a>
